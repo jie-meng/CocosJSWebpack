5c27a6cbb18ef24534c01ccfc4392a03d69a80bc
Jie Meng <jiemeng@thoughtworks.com>
2017-08-17 23:24:10 +0800

    Add LICENSE

--- src/utils/Calculation.js
+++ src/utils/Calculation.js
@@ -3,18 +3,18 @@ let Calculation = cc.Class.extend({
 
 Calculation.add = (a, b) => {
     return a + b;
-}
+};
 
 Calculation.minus = (a, b) => {
     return a - b;
-}
+};
 
 Calculation.multiple = (a, b) => {
     return a * b;
-}
+};
 
 Calculation.divide = (a, b) => {
     return a / b;
-}
+};
 
 export default Calculation;
